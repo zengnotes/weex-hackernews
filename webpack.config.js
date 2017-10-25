aba4d76cea1c318607d049ce173c5a246b4e5367
zeng <asd@12.com>
2017-10-25 00:12:20 -0400

    修改项目

--- webpack.config.js
+++ webpack.config.js
@@ -2,34 +2,37 @@ var path = require('path')
 var webpack = require('webpack')
 
 var bannerPlugin = new webpack.BannerPlugin({
-  banner: '// { "framework": "Vue" }\n',
-  raw: true
+    banner: '// { "framework": "Vue" }\n',
+    raw: true
 })
 
-function getBaseConfig () {
-  return {
-    entry: {
-      'index': path.resolve('src', 'entry.js')
-    },
-    output: {
-      path: path.resolve(__dirname, 'dist')
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          loader: 'babel-loader',
-          exclude: /node_modules/
-        }, {
-          test: /\.vue(\?[^?]+)?$/
-        }
-      ]
-    },
-    plugins: [
-      // new webpack.optimize.UglifyJsPlugin({compress: { warnings: false }}),
-      bannerPlugin
-    ]
-  }
+function getBaseConfig() {
+    return {
+        entry: {
+            'index': path.resolve('src', 'entry.js')
+        },
+        output: {
+            path: path.resolve(__dirname, 'dist')
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['es2015']
+                    },
+                    exclude: /node_modules/
+                }, {
+                    test: /\.vue(\?[^?]+)?$/
+                }
+            ]
+        },
+        plugins: [
+            //new webpack.optimize.UglifyJsPlugin({compress: { warnings: false }}),
+            bannerPlugin
+        ]
+    }
 }
 
 var webConfig = getBaseConfig()
@@ -40,14 +43,14 @@ webConfig.module.rules[1].loader = 'vue-loader'
  * inline style normalization.
  */
 webConfig.module.rules[1].options = {
-  compilerModules: [
-    {
-      postTransformNode: el => {
-        el.staticStyle = `$processStyle(${el.staticStyle})`
-        el.styleBinding = `$processStyle(${el.styleBinding})`
-      }
-    }
-  ]
+    compilerModules: [
+        {
+            postTransformNode: el => {
+                el.staticStyle = `$processStyle(${el.staticStyle})`
+                el.styleBinding = `$processStyle(${el.styleBinding})`
+            }
+        }
+    ]
 }
 
 var nativeConfig = getBaseConfig()
